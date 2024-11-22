f1413d81ad12c3454ef9b066858c0e135fd7dc80
17Shivansh <17shivanshsrivastava@gmail.com>
2024-11-22 18:00:20 +0530

    user

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,3 +11,4 @@ export default defineConfig({
     port: 5500, // Replace 4000 with your desired port number
   },
 })
+
